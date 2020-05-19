96c0df9d3ab0e22cfd08f585a4bea96d509ccac4
ftx14 <aakubrakov@yandex.com>
2020-05-19 19:02:17 +0300

    create simple rsocket server

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+group = "com.ftx.h1.outside"
+version = "1.0-SNAPSHOT"
+
+plugins {
+    id("org.springframework.boot") version "2.2.2.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    kotlin("jvm") version "1.3.72"
+    kotlin("plugin.spring") version "1.3.72"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-rsocket")
+    implementation("io.github.microutils:kotlin-logging:1.7.9")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude("org.junit.vintage", "junit-vintage-engine")
+    }
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.4")
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
---
+++ src/main/kotlin/com/ftx/h1/outside/controller/RSocketController.kt
@@ -0,0 +1,13 @@
+package com.ftx.h1.inside.controller
+
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.stereotype.Controller
+import reactor.core.publisher.Mono
+
+@Controller
+class RSocketController {
+
+    @MessageMapping("hands")
+    /* suspend */ fun consumeHand(hm2Hand: String): Mono<String> = Mono.just("received:$hm2Hand")
+
+}
